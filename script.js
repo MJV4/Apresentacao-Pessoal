35e2ae53952b55e75b6236465f99f56b0b9fa2fa
MJV4 <leonardoheizo@gmail.com>
2025-11-16 15:14:18 -0300

    Updating CSS and adding Script

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelectorAll("a[href^='#']").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+const sections = document.querySelectorAll("section");
+
+function revealOnScroll() {
+  sections.forEach((sec) => {
+    const react = sec.getBoundingClientReact();
+    if (react.top < window.innerHeight - 100) {
+      sec.style.opacity = "1";
+      sec.style.transform = "translateY(0)";
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+sections.forEach((sec) => {
+  sec.style.opacity = "0";
+  sec.style.transform = "translateY(40px)";
+  sec.style.transition = "0.8s";
+});
+
+revealOnScroll();
